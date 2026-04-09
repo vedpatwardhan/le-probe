b07d9ef6195a8e3b46bd57b6479fc79dbd0c1aad
vedpatwardhan <vedpat3@gmail.com>
2026-04-09 21:03:16 +0530

    using float32 for svd stability

--- research/train_gr1.py
+++ research/train_gr1.py
@@ -79,10 +79,12 @@ def lejepa_forward(self, batch, stage, cfg):
     tgt_emb = emb[:, n_preds:]  # label
     pred_emb = self.model.predict(ctx_emb, ctx_act)  # pred
 
-    # LeWM loss
+    # LeWM loss (Force SIGReg to float32 for SVD stability)
     output["pred_loss"] = (pred_emb - tgt_emb).pow(2).mean()
-    output["sigreg_loss"] = self.sigreg(emb.transpose(0, 1))
-    output["loss"] = output["pred_loss"] + lambd * output["sigreg_loss"]
+    output["sigreg_loss"] = self.sigreg(emb.float().transpose(0, 1))
+    output["loss"] = output["pred_loss"] + lambd * output["sigreg_loss"].to(
+        output["pred_loss"].dtype
+    )
 
     losses_dict = {f"{stage}/{k}": v.detach() for k, v in output.items() if "loss" in k}
     self.log_dict(losses_dict, on_step=True, sync_dist=True)
